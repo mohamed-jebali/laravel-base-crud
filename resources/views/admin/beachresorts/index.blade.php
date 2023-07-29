8296d8556bc16890f494cec81c4fc494bb532f2b
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-07-29 11:30:47 +0200

    added deletee

--- resources/views/admin/beachresorts/index.blade.php
+++ resources/views/admin/beachresorts/index.blade.php
@@ -27,7 +27,7 @@
                 <form class='delete-button' action="{{ route ('admin.beachresorts.destroy', $beach->id) }}" method='POST'>
                       @csrf
                       @method('DELETE')
-                      <button type="submit" class="btn btn-outline-primary">Deletee</button>
+                      <button type="submit" class="btn btn-outline-primary">Delete</button>
                   </form>
             </div>
             </div>
